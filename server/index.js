7f5949067f57a0f01d35c5e1d1bcbd4c0f048a24
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-30 15:35:53 -0600

    more tests

--- server/index.js
+++ server/index.js
@@ -5,12 +5,16 @@ session = require('express-session'),
 passport = require('passport'),
 Auth0Strategy = require('passport-auth0'),
 massive = require('massive'),
+AWS = require('aws-sdk'),
 port = 3005;
 
 require('dotenv').config();
 const app = express();
+const s3 = new AWS.S3();
 
 app.use(bodyParser.json());
+
+
 
 
 
