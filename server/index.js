95c232f0a366c895bef8bb049a692cadc5c925e2
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-30 12:10:14 -0600

    added basic server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+cors = require('cors'),
+bodyParser = require('body-parser'),
+session = require('express-session'),
+passport = require('passport'),
+Auth0Strategy = require('passport-auth0'),
+massive = require('massive'),
+port = 3005;
+
+require('dotenv').config();
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+app.listen(port, ()=> console.log('Listening on port: ' + port))
